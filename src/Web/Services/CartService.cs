97e3a8c7a705955c1a74b65172d76b150d63e15e
truonghoangduy <truonghoangduy03111999@gmail.com>
2021-12-31 01:39:23 +0700

    add method override

--- src/Web/Services/CartService.cs
+++ src/Web/Services/CartService.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Web.Services.Cart
+namespace Web.Services
 {
     public class CartService : ICartService
     {
@@ -18,9 +18,9 @@ namespace Web.Services.Cart
             _context = context;
         }
 
-        public Order Info(AppUser user)
+        public ApplicationCore.Entities.Cart Info(AppUser user)
         {
-            var query = _context.Orders.Include(order => order.OrderDetails).ThenInclude(orderDetail => orderDetail.Product)
+            var query = _context.Carts.Include(order => order.CartDetails).ThenInclude(orderDetail => orderDetail.Product)
             .FirstOrDefault(order => order.AppUserID == user.Id);
             return query;
         }
@@ -28,7 +28,7 @@ namespace Web.Services.Cart
         /// List<OrderDetail> orderDetails gather form HTTP Controller (cookie, session)
         /// ClaimsPrincipal userContext gather by HTTP Controller HTTP Context by Authorize URL 
         public async Task UpdateCart(AppUser user,
-        OrderDetail addedItem, CartAction action
+        CartDetail addedItem, CartAction action
         // List<OrderDetail> orderDetails
         )
         {
@@ -36,20 +36,20 @@ namespace Web.Services.Cart
             if (order == null)
                 order = await CreateCart(user);
 
-            addedItem.Order = order;
-            addedItem.Order.ID = order.ID;
+            addedItem.Cart = order;
+            addedItem.Cart.ID = order.ID;
 
             // empty user data
-            var existItemIndex = isExist(addedItem.ProductID, order.OrderDetails);
+            var existItemIndex = isExist(addedItem.ProductID, order.CartDetails);
 
             // update / replace
             if (existItemIndex == -1)
             {
-                _context.OrderDetails.Add(addedItem);
+                _context.CartDetails.Add(addedItem);
             }
             else
             {
-                var orderDetail = _context.OrderDetails.FirstOrDefault(order => order.ID == existItemIndex);
+                var orderDetail = _context.CartDetails.FirstOrDefault(order => order.ID == existItemIndex);
 
                 if (action == CartAction.Add_To_Cart)
                 {
@@ -65,13 +65,13 @@ namespace Web.Services.Cart
 
         }
 
-        private void AddToCart(OrderDetail orderDetail)
+        private void AddToCart(CartDetail orderDetail)
         {
             orderDetail.Quantity++;
-            _context.OrderDetails.Update(orderDetail);
+            _context.CartDetails.Update(orderDetail);
         }
 
-        private async Task ModifyOrderDetail(Order order, OrderDetail addedItem, OrderDetail store)
+        private async Task ModifyOrderDetail(ApplicationCore.Entities.Cart order, CartDetail addedItem, CartDetail store)
         {
             if (addedItem.Quantity <= 0)
             {
@@ -85,31 +85,31 @@ namespace Web.Services.Cart
             else
             {
                 store.Quantity = addedItem.Quantity;
-                _context.OrderDetails.Update(store);
+                _context.CartDetails.Update(store);
             }
-            _context.OrderDetails.Update(store);
+            _context.CartDetails.Update(store);
         }
 
-        public async Task Remove(Order order, int productId)
+        public async Task Remove(ApplicationCore.Entities.Cart order, int productId)
         {
-            var orderRemoved = order.OrderDetails.FirstOrDefault(orderDetail => orderDetail.Product.ID == productId);
-            _context.OrderDetails.Remove(orderRemoved);
+            var orderRemoved = order.CartDetails.FirstOrDefault(orderDetail => orderDetail.Product.ID == productId);
+            _context.CartDetails.Remove(orderRemoved);
             await _context.SaveChangesAsync();
         }
 
 
         public async Task Remove(AppUser userContext, int productId)
         {
-            var order = Info(userContext);
-            var orderRemoved = order.OrderDetails.FirstOrDefault(orderDetail => orderDetail.Product.ID == productId);
-            _context.OrderDetails.Remove(orderRemoved);
+            var cart = Info(userContext);
+            var cartRemoved = cart.CartDetails.FirstOrDefault(orderDetail => orderDetail.Product.ID == productId);
+            _context.CartDetails.Remove(cartRemoved);
             await _context.SaveChangesAsync();
             // var user = await _userService.GetByContext(userContext);
             // _context.OrderDetails.Remove(addedItem);
 
         }
 
-        private int isExist(int productId, List<OrderDetail> orderDetails)
+        private int isExist(int productId, List<CartDetail> orderDetails)
         {
             if (orderDetails == null)
                 return -1;
@@ -120,46 +120,16 @@ namespace Web.Services.Cart
             return orderDetailRef.ID;
         }
 
-        private async Task<Order> CreateCart(AppUser user)
+        private async Task<ApplicationCore.Entities.Cart> CreateCart(AppUser user)
         {
-            var order = new Order()
+            var cart = new ApplicationCore.Entities.Cart()
             {
                 AppUser = user,
-                OrderDetails = new List<OrderDetail>(),
+                CartDetails = new List<CartDetail>(),
             };
-            var result = await _context.Orders.AddAsync(order);
+            var result = await _context.Carts.AddAsync(cart);
             _context.SaveChanges();
-            return order;
+            return cart;
         }
-
-        public async Task<Order> Checkout()
-        {
-            return null;
-        }
-
-        // public void UpdateCart()
-        // {
-
-        // }
-
-        // // Remove Product when Quantity <= 0
-        // if (addedItem.Quantity <= 0)
-        // {
-        //     await Remove(order, addedItem.ProductID);
-        // }
-        // else
-        // {
-        //     // Update via Quantity
-        //     orderDetail.Quantity++;
-        //     _context.OrderDetails.Update(orderDetail);
-        // }
-
-
-
-
-
-
-
-
     }
 }
